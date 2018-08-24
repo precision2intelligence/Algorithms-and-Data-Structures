1dbf237e157eec33f47b152b458b405139b0c65d
precision2intelligence <yangqiuxia@bit.edu.cn>
2018-08-24 12:35:46 +0800

    Merge Intervals

---
+++ LeetCode/56. Merge Intervals.py
@@ -0,0 +1,28 @@
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+# 先排序，这样控制住了左边的索引大于右边。
+
+class Solution:
+    def merge(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: List[Interval]
+        """
+        if len(intervals)<=1:
+            return intervals
+        res = []
+        intervals = sorted(intervals, key=lambda intervals:intervals.start)
+        m = len(intervals)
+        i = 0
+        while i < m:
+            start_id = intervals[i].start
+            end_id = intervals[i].end
+            while i < m and intervals[i].start <= end_id:
+                end_id = max(end_id,intervals[i].end)
+                i += 1
+            res.append([start_id,end_id])
+        return res
