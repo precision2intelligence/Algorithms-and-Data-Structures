016c8d8bc38318d877fad915214ad219d62aa64b
precision2intelligence <yangqiuxia@bit.edu.cn>
2018-08-29 09:55:31 +0800

    heap sort & print heap

--- 剑指offer/32.从上到下打印二叉树.py
+++ 剑指offer/32.从上到下打印二叉树.py
@@ -1,31 +1,19 @@
-# -*- coding:utf-8 -*-
-def head_sort(list):
-    length_list = len(list)
-    first = int(length_list / 2 - 1)
-    for start in range(first, -1, -1):
-        max_heapify(list, start, length_list - 1)
-    for end in range(length_list - 1, 0, -1):
-        list[end], list[0] = list[0], list[end]
-        max_heapify(list, 0, end - 1)
-    return list
-
-def max_heapify(ary, start, end):
-    root = start
-    while True:
-        child = root * 2 + 1
-        if child > end:
-            break
-        if child + 1 <= end and ary[child] < ary[child + 1]:
-            child = child + 1
-        if ary[root] < ary[child]:
-            ary[root], ary[child] = ary[child], ary[root]
-            root = child
-        else:
-            break
-
-if __name__=='__main__':
-    a=[10, 23, 1, 53, 654, 54, 16, 646, 65, 3155, 546, 31]
-    head_sort(a)
-    print(head_sort(a))
-
-
+class TreeNode():
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
+def function(root):
+    A = []
+    result = []
+    if not root:
+        return result
+    A.append(root)
+    while A:
+        current_root = A.pop(0)
+        result.append(current_root.val)
+        if current_root.left:
+            A.append(current_root.left)
+        if current_root.right:
+            A.append(current_root.right)
+    return result
