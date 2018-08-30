e8666fc60ba8a03e76204aa996f6380f3e379aa3
precision2intelligence <yangqiuxia@bit.edu.cn>
2018-08-30 17:27:51 +0800

    70. Climbing Stairs

---
+++ LeetCode/70. Climbing Stairs.py
@@ -0,0 +1,24 @@
+#这里和编辑距离一样，是定义一个空表格计算距离，所以一定要初始化list为0
+#定义的dp[i]是第i个字符的步数，而不是i-1,边界要清楚
+#虽然是动规，考虑最后一步，要从前面写
+#开始的时候特例要存储，然后要么return，要么if...elif...else，不然if...else...总会进入else，那是主要动规，特例不应该进入
+class Solution:
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        dp = [0] * (n + 1)
+        for i in range(n + 1):
+            if i == 1:
+                dp[i] = 1
+            elif i == 2:
+                dp[i] = 2
+            else:
+                dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+
+s = Solution()
+print(s.climbStairs(2))
+print(s.climbStairs(3))
