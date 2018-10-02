aee864cf2ae33f79dec316a1b1bfea346e555fd4
precision2intelligence <yangqiuxia@bit.edu.cn>
2018-10-02 21:44:02 +0800

    二叉树的深度

---
+++ 剑指offer/55.二叉树的深度.py
@@ -0,0 +1,15 @@
+# 递归
+
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def TreeDepth(self, pRoot):
+        # write code here
+        if not pRoot:
+            return 0
+        left = self.TreeDepth(pRoot.left)
+        right = self.TreeDepth(pRoot.right)
+        return max(left,right) + 1
