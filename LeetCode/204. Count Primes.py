9a8beb5bf9a7c19eb4ed3c21466167f3fa4294ee
precision2intelligence <yangqiuxia@bit.edu.cn>
2018-10-12 14:11:25 +0800

    Update count primes

--- LeetCode/204. Count Primes.py
+++ LeetCode/204. Count Primes.py
@@ -1,31 +1,25 @@
-# -*- coding:utf-8 -*-
-def head_sort(list):
-    length_list = len(list)
-    first = int(length_list / 2 - 1)
-    for start in range(first, -1, -1):
-        max_heapify(list, start, length_list - 1)
-    for end in range(length_list - 1, 0, -1):
-        list[end], list[0] = list[0], list[end]
-        max_heapify(list, 0, end - 1)
-    return list
+#只考虑平方根
+#过滤法，最快
+'''
+Count the number of prime numbers less than a non-negative number, n.
 
-def max_heapify(ary, start, end):
-    root = start
-    while True:
-        child = root * 2 + 1
-        if child > end:
-            break
-        if child + 1 <= end and ary[child] < ary[child + 1]:
-            child = child + 1
-        if ary[root] < ary[child]:
-            ary[root], ary[child] = ary[child], ary[root]
-            root = child
-        else:
-            break
+Example:
 
-if __name__=='__main__':
-    a=[10, 23, 1, 53, 654, 54, 16, 646, 65, 3155, 546, 31]
-    head_sort(a)
-    print(head_sort(a))
-
-
+Input: 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+'''
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 1:
+            return 0
+        primes = [True for _ in range(n)]
+        primes[0] = primes[1] = False
+        for i in range(2,int(n**0.5)+1):
+            if primes[i]:
+                primes[i*i:n:i] = [False] * len(primes[i*i:n:i])
+        return sum(primes)
