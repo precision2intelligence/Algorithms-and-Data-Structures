d15ead282c2783951813f504f84f12150028651e
precision2intelligence <yangqiuxia@bit.edu.cn>
2018-10-19 20:14:05 +0800

    合并两个排序的链表

---
+++ 剑指offer/25.合并两个排序的链表.py
@@ -0,0 +1,21 @@
+#一定用递归，不然太low
+#特例是有空指针，不能让程序崩溃
+
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    # 返回合并后列表
+    def Merge(self, pHead1, pHead2):
+        # write code here
+        if not pHead1:
+            return pHead2
+        if not pHead2:
+            return  pHead1
+        if pHead1.val <= pHead2.val:
+            pHead1.next = self.Merge(pHead1.next, pHead2)
+            return pHead1
+        else:
+            pHead2.next = self.Merge(pHead1, pHead2.next)
+            return pHead2
