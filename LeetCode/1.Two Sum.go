898557d6ce7f91cb48877847dd33c9c91759bf19
precision2intelligence <tuanzikuaipao@163.com>
2020-08-06 23:04:52 +0800

    modified README & start go

---
+++ LeetCode/1.Two Sum.go
@@ -0,0 +1,28 @@
+// Time: O(n)
+// Space: O(1)
+
+/*
+牛客上要求索引顺序递增。
+
+Given an array of integers, find two numbers such that they add up to a specific target number.
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+You may assume that each input would have exactly one solution.
+
+Input: numbers={2, 7, 11, 15}, target=9
+Output: index1=1, index2=2
+
+*/
+
+// 如果字符串有序，则双指针。167题
+
+func twoSum(numbers []int, target int) []int {
+	hash := make(map[int]int, len(numbers))
+	for k, v := range numbers {
+		if v, ok := hash[target-v]; ok {
+			return []int{v + 1, k + 1}
+		}
+		hash[v] = k
+	}
+	return nil
+}
