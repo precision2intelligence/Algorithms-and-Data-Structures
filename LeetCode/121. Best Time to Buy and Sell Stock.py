0b2cf1b3022e861548390a09dc0e9193c3cb6f3d
precision2intelligence <tuanzikuaipao@163.com>
2020-12-12 12:29:30 +0800

    update DP solution

--- LeetCode/121. Best Time to Buy and Sell Stock.py
+++ LeetCode/121. Best Time to Buy and Sell Stock.py
@@ -1,6 +1,8 @@
 # Time:  O(n)
 # Space: O(1)
 
+# 更新动规写法
+
 #本质是找到最大最小值之间的差值，但是双层循环会超时
 #保存最小买入时机和当前最大收益，如果大于买入时机则判断是否更新收益，小于则更新最小买入，收益只有超过之前才更新
 #输入为空或者长度为1的情况要返回‘0’
@@ -13,6 +15,7 @@ If you were only permitted to complete at most one transaction (i.e., buy one an
 
 Note that you cannot sell a stock before you buy one.
 '''
+# 解法一：贪心？
 class Solution(object):
     def maxProfit(self, prices):
         """
@@ -29,3 +32,26 @@ class Solution(object):
             elif prices[i] < minPrice:
                 minPrice = prices[i]
         return maxProfile
+
+# 解法二：动规
+class Solution:
+    def maxProfit(self, prices):
+        m = len(prices)
+        if m < 2:
+            return 0
+        dp = [0, -prices[0]]
+        for i in range(1, m):
+            dp[0] = max(dp[0], dp[1] + prices[i])
+            # 易错：只能买卖一次，所以这里直接是-prices[i]，不需要前一天不持有股票转移过来，这会造成多次交易
+            dp[1] = max(dp[1], -prices[i])
+        '''
+        未优化空间的动规
+        dp = [[0, -prices[0]]] + [[0] * 2 for _ in range(m-1)]
+        for i in range(1, m):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+            # dp[i-1][1] = max(dp[i-1][0]-prices[i], dp[i-1][1])
+            dp[i][1] = max(dp[i - 1][1], -prices[i])
+        return dp[m-1][0]
+        '''
+
+        return dp[0]
