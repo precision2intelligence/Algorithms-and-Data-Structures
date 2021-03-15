d029a3d099b3af4929fadf1c6a9e8ee40a39e30b
precision2intelligence <tuanzikuaipao@163.com>
2021-03-15 16:33:18 +0800

    update

--- LeetCode/78. Subsets.py
+++ LeetCode/78. Subsets.py
@@ -22,26 +22,23 @@
 # ]
 
 
-class Solution:
+class Solution(object):
     def subsets(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        if not nums:
-            return
-        def dfs(lists,idx,subs,results):
-            results.append(subs)
-            for i in range(idx,len(lists)):
-                dfs(lists,i+1,subs+[lists[i]],results)
-                #这里有个不易发现的错误：dfs(lists,i+1,subs.append(lists[i]),results)
-                #如果用了append函数，那么subs会变成‘None’
-                #eg. a = [45]  a.append(89) >>>  print(a) 结果是[45,89] ;print(a.append(89)) 结果是None
-                #eg. a = [45] a + 89 会报错；a + [89] >> a = [45],a+ [89] 结果是[45,89], a的值不会改变
-        res = []
-        sub = []
-        dfs(nums,0,sub,res)
+        m = len(nums)
+        res = [[]]
+        for i in range(m):
+            newarr = []
+            dfs(newarr, res, i, m, nums)
         return res
-if __name__ == '__main__':
-    solu = Solution()
-    print(solu.subsets([1,2,3]))
+
+def dfs(newarr, res, i, m, nums):
+    if i == m:
+            return
+    newarr = newarr + [nums[i]]
+    res.append(newarr)
+    for j in range(i+1,m):
+        dfs(newarr, res, j, m, nums)
