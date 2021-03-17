5ad86debba90b093a9400255a189049a9ebe7f1d
precision2intelligence <tuanzikuaipao@163.com>
2021-03-17 21:57:39 +0800

    labuladong修正

--- LeetCode/213. House Robber II.py
+++ LeetCode/213. House Robber II.py
@@ -1,8 +1,9 @@
 #Time O(n)
-#Space O(n)
+#Space O(1)
 
+#简化空间复杂度
 #按照是否偷第一家分类
-#注意如果有两家，偷其中一家不想警报
+#注意如果有两家，偷其中一家不警报
 
 '''
 You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
@@ -23,30 +24,19 @@ Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
              Total amount you can rob = 1 + 3 = 4.
 '''
 
-#解法一：dp
 class Solution(object):
     def rob(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        if not nums:
-            return 0
-        elif len(nums) <= 2:
+        if len(nums) == 2:
             return max(nums)
-        return max(nums[0]+self.dp_rob(nums[2:-1]), self.dp_rob(nums[1:]))
-    def dp_rob(self, nums):
-        if not nums:
-            return 0
-        elif len(nums) == 1:
-            return nums[0]
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
-        dp[1] = max(nums[1], nums[0])
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
-        return dp[-1]
+        return max(nums[0]+dp_rob(nums[2:-1]), dp_rob(nums[1:]))
+def dp_rob(nums):
+    last, now = 0, 0
+    for num in nums:
+        last, now = now, max(num+last, now)
+    return now
 
-s = Solution()
-print(s.rob([1,1]))
 
