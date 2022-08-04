1e9d0f80c4120fb680fbf8af7d56170ebebef9e1
precision2intelligence <tuanzikuaipao@163.com>
2022-08-04 23:03:08 +0800

    21. Merge Two Sorted Lists

---
+++ LeetCode/21. Merge Two Sorted Lists.py
@@ -0,0 +1,59 @@
+#Time: O(m+n)
+#Space: O(1)
+
+#思路：
+#双指针
+#返回首节点就返回了全部链表
+
+'''
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode(-1)
+        p = head
+        while list1 and list2:
+            if list1.val < list2.val:
+                p.next = list1
+                list1 = list1.next
+            else:
+                p.next = list2
+                list2 = list2.next
+            p = p.next
+        p.next = list1 if list2 is None else list2
+
+        return head.next
