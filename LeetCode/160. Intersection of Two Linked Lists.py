26ca67b571a2fda3cc2cdc67d90762059146587d
precision2intelligence <tuanzikuaipao@163.com>
2022-11-12 21:55:51 +0800

    26. Remove Duplicates from Sorted Array

--- LeetCode/160. Intersection of Two Linked Lists.py
+++ LeetCode/160. Intersection of Two Linked Lists.py
@@ -1,6 +1,9 @@
 #Time: O(m+n)
 #Space: O(1)
 
+# 简单问题注意举例特殊情况来验证边界
+# 如这道题，要想到不相交的情况，避免进入一直交换指针永远同时走不到None节点的情况
+
 # 统计两个链表的长度，去掉长列表的长度，然后遍历知道找到相同的节点
 '''
 Write a program to find the node at which the intersection of two singly linked lists begins.
@@ -30,6 +33,42 @@ Your code should preferably run in O(n) time and use only O(1) memory.
 #         self.val = x
 #         self.next = None
 
+# 解法一：双指针法，核心在于构造共同前进距离，所以两个指针都有两个长度；两个链表如果相交，会相遇在交点；不相交，会走完两个链表长度到达None
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        pa, pb = headA, headB
+        while pa != pb:
+            if pa == None:
+                pa = headB
+            else:
+                pa = pa.next
+
+            if pb == None:
+                pb = headA
+            else:
+                pb = pb.next
+        return pa
+
+# 解法二：同解法一，就是避免出错，反复check两个节点是否相等（移动前比较，移动后比较）
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        pa, pb = headA, headB
+        while pa != pb: # 第一次比较
+
+            pa = pa.next
+            pb = pb.next
+            if pa == pb: # 第二次比较，移动后马上比较
+                break
+            if pa == None:
+                pa = headB
+            if pb == None:
+                pb = headA
+
+        return pa
+
+
+# 解法三：先取得两链表长度，长链表节点先走差值，再并进。缺点是遍历两遍
 class Solution(object):
     def getIntersectionNode(self, headA, headB):
         """
